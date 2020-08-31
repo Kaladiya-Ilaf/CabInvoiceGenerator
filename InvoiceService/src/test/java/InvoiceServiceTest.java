a7a110d3acdfe680c905aae8b5739b3f5b784bcc
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-08-31 08:21:08 +0530

    Added : Test and Code to check total fare of multiple rides.

--- InvoiceService/src/test/java/InvoiceServiceTest.java
+++ InvoiceService/src/test/java/InvoiceServiceTest.java
@@ -19,4 +19,15 @@ public class InvoiceServiceTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0.0);
     }
+
+    @Test
+    public void givenMultipleRides_sholudReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {new Ride(2.0, 5),
+                        new Ride(0.1, 5),
+                        new Ride(0.1,1)
+                        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(36, fare, 0.0);
+    }
 }
